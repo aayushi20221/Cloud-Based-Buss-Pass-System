a349069b5b7930a921acbfc806ec73d1ee7dc38e
saksham8979 <sakshamjohri@gmail.com>
2019-12-01 13:33:25 +0530

    Update renew.php

--- renew.php
+++ renew.php
@@ -255,7 +255,7 @@ span.price {
           <div class="col-lg-4">
             <div class="mb-5">
               <h3 class="footer-heading mb-4">About Travelers</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque, consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima minus odio!</p>
+              <p>Provides student a pass for their daily life to travel to/from GLA University, Mathura</p>
             </div>
 
             
